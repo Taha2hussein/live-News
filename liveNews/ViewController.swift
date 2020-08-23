c9598259ca088fdfc51dc5053cd15e74a345813f
Taha2hussein <taha.hussein.two@gmail.com>
2020-08-23 03:12:56 +0200

    Initial Commit

---
+++ liveNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  liveNews
+//
+//  Created by A on 8/23/20.
+//  Copyright © 2020 Taha Hussein. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
